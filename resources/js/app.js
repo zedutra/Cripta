a39d6e4630d03c66d6ad0745b2e57ded9aac6a72
zedutra <zedutra93@gmail.com>
2024-07-24 01:35:12 -0300

    substituindo IonIcons por Iconify

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,17 +2,19 @@ import '../css/app.css'
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { Icon } from '@iconify/vue';
 
 createInertiaApp({
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
     return pages[`./Pages/${name}.vue`]
   },
-  title: title => title ? `${title} - Ping CRM` : 'Ping CRM',
+  title: title => title ? `${title} - CRIPTA` : 'CRIPTA',
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
       .use(ZiggyVue)
+      .component('Icon', Icon)
       .mount(el)
   },
 })
